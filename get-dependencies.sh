dddf668e5bfaad2d397e4a10ccc08426eb0c904e
Link4Electronics <link4electronics@gmail.com>
2026-04-10 17:02:38 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -18,7 +18,8 @@ get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
 make-aur-package gtk2
-PRE_BUILD_CMDS="export CXXFLAGS=\"\$CXXFLAGS -fpermissive\"" make-aur-package qt4
+make-aur-package qt4
+#PRE_BUILD_CMDS="export CXXFLAGS=\"\$CXXFLAGS -fpermissive\"" make-aur-package qt4
 
 # If the application needs to be manually built that has to be done down here
 echo "Making nightly build of Glypha..."
