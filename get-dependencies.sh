df0eee79e6412952f19d97de434cc636d47d8bf9
Link4Electronics <link4electronics@gmail.com>
2026-04-10 08:02:34 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,13 +6,18 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+    cups            \
+    mariadb-libs    \
+    postgresql-libs \
+    unixodbc
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
+make-aur-package gtk2
 make-aur-package qt4
 
 # If the application needs to be manually built that has to be done down here
