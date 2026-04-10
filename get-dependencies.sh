47af49e22aee4d00b6afcf358017310ecf1fc6ea
Link4Electronics <link4electronics@gmail.com>
2026-04-10 18:30:01 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,6 +7,7 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
+    cmake           \
     cups            \
     mariadb-libs    \
     postgresql-libs \
